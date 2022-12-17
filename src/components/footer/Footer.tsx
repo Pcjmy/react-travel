b4e2d023095120dee2d47806437027c5a3cbb164
Pcjmy <1716045191@qq.com>
2022-12-17 17:40:12 +0800

    走马灯与侧边栏

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import styles from './Footer.module.css'
 import { Layout, Typography } from 'antd';
 
 export const Footer: React.FC = () => {
   return (
-    <Layout.Footer className={styles['main-footer']}>
+    <Layout.Footer>
       <Typography.Title level={3} style={{textAlign: 'center'}}>
         版权所有@React 旅游网
       </Typography.Title>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export * from './header'
 export * from './footer'
+export * from './carousel'
+export * from './sideMenu'

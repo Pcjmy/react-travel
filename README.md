f71439da1d54a13ac92e0d17d8d2360145a16b6e
Pcjmy <1716045191@qq.com>
2022-12-18 14:36:11 +0800

    路由初始化

--- README.md
+++ README.md
@@ -5,7 +5,16 @@
 ```shell
 npx create-react-app react-travel --template typescript
 npm install antd @ant-design/icons
+npm install react-router-dom@5.2.0
+npm install @types/react-router-dom --save-dev
 ```
 
 UI框架
 [Ant Design](https://ant.design/index-cn)
+
+### 遇到的问题
+
+```shell
+1.Error: A <Route> is only ever to be used as the child of <Routes> element, never rendered directly. Please wrap your <Route> in a <Routes>.
+解决办法：修改react-router-dom版本
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,13 @@
 import styles from './App.module.css';
-import { Header, Footer, SideMenu, Carousel, ProductCollection, BusinessPartners } from './components';
-import { Typography } from 'antd';
-import { Row, Col } from 'antd';
-import { productList1, productList2, productList3 } from './mockups';
-import sideImage from './assets/images/sider_2019_12-09.png';
-import sideImage2 from './assets/images/sider_2019_02-04.png';
-import sideImage3 from './assets/images/sider_2019_02-04-2.png';
+import { BrowserRouter, Route } from 'react-router-dom'
+import { HomePage } from './pages'
 
 function App() {
   return (
     <div className={styles.App}>
-      <Header />
-      <div className={styles['page-content']}>
-        <Row style={{marginTop: 20}}>
-          <Col span={6}>
-            <SideMenu />
-          </Col>
-          <Col span={18}>
-            <Carousel />
-          </Col>
-        </Row>
-        <ProductCollection
-          title={<Typography.Title level={3} type="warning">爆款推荐</Typography.Title>}
-          sideImage={sideImage}
-          products={productList1}
-        />
-        <ProductCollection
-          title={<Typography.Title level={3} type="danger">新品上市</Typography.Title>}
-          sideImage={sideImage2}
-          products={productList2}
-        />
-        <ProductCollection
-          title={<Typography.Title level={3} type="success">新品上市</Typography.Title>}
-          sideImage={sideImage3}
-          products={productList3}
-        />
-        <BusinessPartners />
-      </div>
-      <Footer />
+      <BrowserRouter>
+        <Route path="/" component={ HomePage } />
+      </BrowserRouter>
     </div>
   );
 }

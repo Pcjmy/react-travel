57b6556cc4c0efe6a82edf67d103b9788665ff7a
Pcjmy <1716045191@qq.com>
2023-02-25 00:55:59 +0800

    feat: react router不同组件间数据传递的方法

--- src/components/productCollection/ProductImage.tsx
+++ src/components/productCollection/ProductImage.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Image, Typography } from "antd";
+import { Image, Typography } from 'antd';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 
-interface PropsType {
+interface PropsType extends RouteComponentProps {
     id: string | number;
     size: "large" | "small";
     imageSrc: string;
@@ -9,9 +10,21 @@ interface PropsType {
     title: string;
 }
 
-export const ProductImage:React.FC<PropsType> = ({ id, size, imageSrc, price, title }) => {
+const ProductImageComponent: React.FC<PropsType> = ({
+    id,
+    size,
+    imageSrc,
+    price,
+    title,
+    history,
+    location,
+    match
+}) => {
+    // console.log(history)
+    // console.log(location)
+    // console.log(match)
     return (
-        <>
+        <div onClick={() => history.push(`detail/${id}`)}>
             {size === "large" ? (
                 <Image src={imageSrc} height={285} width={490} />
             ) : (
@@ -25,6 +38,8 @@ export const ProductImage:React.FC<PropsType> = ({ id, size, imageSrc, price, ti
                     ￥ {price} 起
                 </Typography.Text>
             </div>
-        </>
+        </div>
     );
 }
+
+export const ProductImage = withRouter(ProductImageComponent)

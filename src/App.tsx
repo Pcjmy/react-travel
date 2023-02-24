e30fe079d1a89652d2daff8d710e438ce341e84c
Pcjmy <1716045191@qq.com>
2023-02-25 01:23:18 +0800

    feat: 使用hooks传递路由数据

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import styles from './App.module.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { HomePage } from './pages'
+import React from 'react';
+import styles from "./App.module.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HomePage,SignInPage,RegisterPage,DetailPage } from './pages';
 
 function App() {
-  return (
-    <div className={styles.App}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ HomePage } />
-          <Route path="/login" render={() => <h1>登录页面</h1>} />
-          <Route render={() => <h1>404 not found 页面去火星了！</h1>}/>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className={styles.App}>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/signIn" component={SignInPage} />
+                    <Route path="/register" component={RegisterPage} />
+                    <Route path="/detail/:touristRouteId" component={DetailPage} />
+                    <Route render={() => <h1>404  not found 页面去火星了！</h1>} />
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;

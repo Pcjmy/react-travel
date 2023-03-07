ba1217d083a1bc238dcbdc5ac4d17e9a39a3cd1f
Pcjmy <1716045191@qq.com>
2023-03-07 22:19:32 +0800

    feat: 安装Redux

---
+++ src/redux/languageReducer.ts
@@ -0,0 +1,14 @@
+interface LanguageState{
+  language:"en"|"zh";
+  languageList:{name:string,code:string}[];
+}
+const defaultState:LanguageState={
+  language:"zh",
+  languageList:[
+      {name:"中文",code:"zh"},
+      {name:"English",code:"en"},
+  ],
+};
+export default(state=defaultState,action)=>{
+  return state
+};
